694e1dce6d1f6e774d87ab1bc6038475a75bd21c
higopika <higopika@gmail.com>
2023-08-06 19:26:45 +0530

    changed return type from json to string

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
     result = {}
-    result['result'] = "Greetings"
+    result['result'] = "hello world"
     return result
 
 @app.route("/calculator/add", methods=['POST'])
@@ -14,14 +14,14 @@ def add():
     result={}
     numbers = request.get_json()
     result['result'] = int(numbers['first']) + int(numbers['second'])
-    return result
+    return str(result['result'])
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     result = {}
     numbers = request.get_json()
     result['result'] = int(numbers['first']) + int(numbers['second'])
-    return result
+    return str(result['result'])
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
